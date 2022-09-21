7a845a08a35fe45b8b692da49740b50ed1a782a5
Federico Ceballos <ffedericoceballos@gmail.com>
2022-09-21 14:53:53 -0300

    add git

---
+++ index.js
@@ -0,0 +1,15 @@
+var count1= 0
+var count2= 0
+
+
+function changeCount1(num){
+count1+= num
+document.getElementById("count1").innerHTML=count1
+
+}
+
+function changeCount2(num){
+    count2+= num
+    document.getElementById("count2").innerHTML=count2
+    
+    }
